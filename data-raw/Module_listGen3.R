0675169b71c66d26d8919c0818c22ed80ffa4093
Drinchai <drinchai@gmail.com>
2020-07-22 15:03:42 +0300

    Update Module_listGen3.R

--- data-raw/Module_listGen3.R
+++ data-raw/Module_listGen3.R
@@ -3,4 +3,6 @@ Module_listGen3 <- read.csv("data-raw/Module_listGen3.csv",
 
 rownames(Module_listGen3) = Module_listGen3$Module_gene
 
+Module_listGen3 = data.frame(Module_listGen3)
+
 usethis::use_data_raw(Module_listGen3, overwrite = TRUE)
