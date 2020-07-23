89ac21647274799c27cce9840b17f5d55e274adb
Drinchai <drinchai@gmail.com>
2020-07-23 10:22:58 +0300

    Update Gen3_ann.R

--- data-raw/Gen3_ann.R
+++ data-raw/Gen3_ann.R
@@ -5,4 +5,4 @@ Gen3_ann <- read.csv("data-raw/Gen3_ann.csv",
 
 rownames(Gen3_ann) = Gen3_ann$Module
 
-devtools::use_data_raw(Gen3_ann, overwrite = TRUE)
+#devtools::use_data_raw(Gen3_ann, overwrite = TRUE)
