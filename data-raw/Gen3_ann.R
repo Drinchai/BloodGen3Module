63707222f9e4da79a821d2bd3da5c8504625d5e3
Drinchai <drinchai@gmail.com>
2020-11-02 08:56:19 +0300

    Delete Gen3_ann.R

--- data-raw/Gen3_ann.R
+++
@@ -1,8 +0,0 @@
-
-
-Gen3_ann <- read.csv("data-raw/Gen3_ann.csv",
-                                  stringsAsFactor = FALSE)
-
-rownames(Gen3_ann) = Gen3_ann$Module
-
-#devtools::use_data_raw(Gen3_ann, overwrite = TRUE)
