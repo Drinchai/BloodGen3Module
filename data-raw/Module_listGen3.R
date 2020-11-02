d4b049e0639b7c7ad6e20152b17b22d7364f6f38
Drinchai <drinchai@gmail.com>
2020-11-02 08:56:42 +0300

    Delete Module_listGen3.R

--- data-raw/Module_listGen3.R
+++
@@ -1,6 +0,0 @@
-Module_listGen3 <- read.csv("data-raw/Module_listGen3.csv",
-                     stringsAsFactor = FALSE)
-
-rownames(Module_listGen3) = Module_listGen3$Module_gene
-
-#usethis::use_data_raw(Module_listGen3, overwrite = TRUE)
