2a1737651f76f94f202987b7a266c554b6fde86a
Drinchai <drinchai@gmail.com>
2021-04-05 07:37:12 +0300

    updated version

---
+++ R/Data.R
@@ -0,0 +1,19 @@
+#' Functional annotation for visulization
+#'
+#'
+#' @format A data.frame with 6 variables:\code{Modules}, \code{Aggregate=Cluster}, \code{Sub_aggregate=Cluster_location},\code{Function},\code{Position},\code{Module_color}
+"Gen3_ann"
+
+#' Module list member
+#'
+#' A dataframe contain member of 3rd generantion of blood module repertoire construction
+#'
+#' @format A data.frame with 14168 rows by 8 variables:\code{Module}, \code{Gene symbol}, \code{Probe ID}, \code{Module_gene},\code{Functiom},\code{Cluster},\code{Cluster_location},\code{position}
+"Module_listGen3"
+
+#' Color for fingerprint visulization
+#'
+#' Character vector of color for fingerprint plot
+#'
+#' @format A vector of 1134 character
+"color"
