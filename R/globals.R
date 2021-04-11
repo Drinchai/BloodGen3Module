7cf2ae691d7058e227787f9d5237b6cd6b8389e4
Drinchai <drinchai@gmail.com>
2021-04-11 16:37:07 +0300

    Create globals.R

---
+++ R/globals.R
@@ -0,0 +1,3 @@
+utils::globalVariables(c("Gen3_ann","Module_listGen3","color",
+                         "Aggregate", "Sub_aggregate","DataFrame",
+                         "SimpleList", "%Response"))
